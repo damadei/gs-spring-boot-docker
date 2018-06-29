82dfb58cd35f330275b6ef2712bf88e9f6fc1e93
damadei <daniel.amadei@gmail.com>
2018-06-29 13:38:55 -0300

    demo globant

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World on Azure";
+        return "Hello Docker World on Azure!!! - demo Globant";
     }
 
     public static void main(String[] args) {
