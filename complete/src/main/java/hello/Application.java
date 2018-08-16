d7eae8a396dd4b69f08fee410937608d62744be9
damadei <daniel.amadei@gmail.com>
2018-08-16 16:34:01 -0300

    cambio demo accenture

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World on Azure! - build via Jenkins!";
+        return "Hello Docker World on Azure!! - Cambio en Vivo y Build via Jenkins ";
     }
 
     public static void main(String[] args) {
